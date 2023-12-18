e36971714c64305c9ac720e8447d3f2dfc0923e0
aboglion <aboglion@gmail.com>
2023-12-18 15:02:49 +0200

    teat

--- flask_app/TDC_parse_eb/TDC_parse_eb_utils/Consts.py
+++ flask_app/TDC_parse_eb/TDC_parse_eb_utils/Consts.py
@@ -2,7 +2,7 @@
 import os
 defult_STATUS = "existed"
 
-####-====== RUN MODE TOGALS .. =====
+# ####-====== RUN MODE TOGALS .. =====
 # ####[1]-- BYCcompose  http://localhost --
 EB_FILES_DIR="/TDC/DB"
 DB_JASON="/TDC/DB_JASON"
@@ -10,14 +10,14 @@ DB_SQL="/TDC/DB_SQL"   # the folder
 def runmode(app):
     print("SERVER RUNING")
 
-###[2]-- debug_run  http://localhost:5000/ --
+##[2]-- debug_run  http://localhost:5000/ --
 # script_path = ("/").join(os.path.abspath(__file__).split("/")[:-5])
 # EB_FILES_DIR = f"{script_path}/TDC_DE/DB"
 # DB_JASON = f"{script_path}/TDC_DE/DB_JASON"
 # DB_SQL=f"{script_path}/TDC_DE/DB_SQL" # the folder
 # def runmode(app):
 #     app.run(debug=True)
-###------------------------------
+##------------------------------
 
 print("EB_FILES_DIR: ", EB_FILES_DIR)
 print("DB_JASON: ", DB_JASON)
