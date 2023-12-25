b55f0bb2376da8be3cf0598af6b0db2cd8c0ee06
aboglion <53370175+aboglion@users.noreply.github.com>
2023-12-25 09:59:03 +0200

    Update router_sql.py

--- flask_app/router_sql.py
+++ flask_app/router_sql.py
@@ -23,8 +23,8 @@ def GET_file_encoding(file):
     with open(file, 'rb') as f:
         result = chardet.detect(f.read())
         encoding_ = result['encoding']
-        if encoding_ == "utf-8":return "utf-8"
-        else:return "windows-1255"
+    if encoding_ == "utf-8":return "utf-8"
+    else:return "windows-1255"
 
 def copy_file(local=SQLFILE, out=SQLFILE_COPYTO):
     if not os.path.exists(local) and os.path.exists(out):
