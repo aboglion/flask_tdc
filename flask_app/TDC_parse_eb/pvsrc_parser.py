6663fd49e4eb77bd64423b4cfbcd05d3c1a1fa69
aboglion <aboglion@gmail.com>
2024-02-07 13:25:13 +0200

    5555

--- flask_app/TDC_parse_eb/pvsrc_parser.py
+++ flask_app/TDC_parse_eb/pvsrc_parser.py
@@ -86,6 +86,7 @@ def pvsrc_parse():
                     epks_file, "r", encoding='windows-1255').read().splitlines()[startline:]
                 print(datastring)
                 mitkan_epks = epks_file.split("\\")[1].split(" ")[0]
+                print(mitkan_epks)
                 for l in datastring:
 
                     l = l.split(",")
